48b2ccc1190de40c99cb0ea6677fb480aa0b9b9e
Eugene Kharitonov <111708438+ekharitonov@users.noreply.github.com>
2023-09-02 21:23:41 +0000

    Changes to be committed:     new file:   database/documents.db 	new file:   database/test.pdf 	modified:   database/testdb.py

--- database/testdb.py
+++ database/testdb.py
@@ -1,7 +1,8 @@
-# 1. Convert PDFs to Text using pdfplumber
-
 import pdfplumber
+import re
+import sqlite3
 
+# 1. Convert PDFs to Text using pdfplumber
 def extract_text_from_pdf(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         text = ""
@@ -9,22 +10,19 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
     return text
 
-pdf_path = "example.pdf"  # Replace with your PDF file path
+pdf_path = "/workspaces/codespaces-jupyter/database/test.pdf"  # Replace with your PDF file path
 extracted_text = extract_text_from_pdf(pdf_path)
 
 # 2. Data Cleaning
-
-import re
-
 def clean_text(text):
     # Remove headers and footers or any other unwanted text
+    # Replace 'Some Regular Expression' with actual patterns you want to remove
     text = re.sub(r"Some Regular Expression", "", text)
     return text
 
 cleaned_text = clean_text(extracted_text)
 
-# 3. Data Cleaning
-
+# 3. Data Structuring
 data = {
   "documents": [
     {
@@ -35,14 +33,9 @@ data = {
 }
 
 # 4. Store in Database
-## Create the SQLite database and tables
-import sqlite3
-
 def create_database():
     conn = sqlite3.connect("documents.db")
     c = conn.cursor()
-
-    # Create the Documents table
     c.execute("""
         CREATE TABLE IF NOT EXISTS Documents (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -50,21 +43,17 @@ def create_database():
             content TEXT NOT NULL
         );
     """)
-
     conn.commit()
     conn.close()
 
 create_database()
 
-## Insert data into this table
 def insert_document(title, content):
     conn = sqlite3.connect("documents.db")
     c = conn.cursor()
-
     c.execute("INSERT INTO Documents (title, content) VALUES (?, ?)", (title, content))
-
     conn.commit()
     conn.close()
 
 # Example usage:
-# insert_document("Document 1", cleaned_text)
+insert_document("Document 1", cleaned_text)
